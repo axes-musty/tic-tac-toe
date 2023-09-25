a2b5f0172a6c3337c2bb818531d220fe6d5f7a88
Axes Musty <>
2023-09-25 17:07:42 +0200

    Bugfix

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -3,25 +3,27 @@ import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
 
 const Tile = (props) => {
-  const { currentPlayer, dispatch, board } = useContext(AppContext)
+  const { currentPlayer, dispatch, board, gameState } = useContext(AppContext)
   let displayValue = board[props.id]
   const handleClick = (e) => {
-    if (board[props.id] === 'Z') {
-      dispatch({
-        type: 'CHANGE_PLAYER'
-      })
-      dispatch({
-        type: 'SET_GAMESTATE',
-        payload: 1
-      })
-      dispatch({
-        type: 'SET_TILEVALUE',
-        id: props.id,
-        value: currentPlayer
-      })
+    if (gameState === 1 || gameState === 0) {
+      if (board[props.id] === 'Z') {
+        dispatch({
+          type: 'CHANGE_PLAYER'
+        })
+        dispatch({
+          type: 'SET_GAMESTATE',
+          payload: 1
+        })
+        dispatch({
+          type: 'SET_TILEVALUE',
+          id: props.id,
+          value: currentPlayer
+        })
+      } else {
+        alert('Tile occupied!')
+      }
       /* setValue(currentPlayer); */
-    } else {
-      alert('Tile occupied!')
     }
   }
   if (board[props.id] === 'Z') {
